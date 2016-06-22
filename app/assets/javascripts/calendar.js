b811bf0140e1e666c8974cf5f6e3e4b6586ea257
Gonda <toshiki.gonda@avasys.jp>
2016-06-22 23:56:30 +0000

    Gonda v2.1

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -32,9 +32,10 @@ $(document).ready(function() {
             //alert('ウィンドウリサイズイベント');
         },
         // 日付クリックイベント
-        dayClick: function (data, allDay, jsEvent, view) {
-
-            $(this).css('background-color','red');
+        dayClick: function (date, allDay, jsEvent, view) {
+          $(this).css('background-color','red');
+          //var date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1);
+          alert(date.format());
         },
         // 初期表示ビュー
         defaultView: 'month',
