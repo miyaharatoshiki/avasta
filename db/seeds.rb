7dc7532c914436b3a68afe8d2cb0fdda0ca27a49
Gonda <toshiki.gonda@avasys.jp>
2016-06-22 08:35:05 +0000

    Gonda v1.3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Picture.create(picture_title: 'aaaa', user_id: '1', picture_data: 'https://www.pakutaso.com/shared/img/thumb/AME84_ganmineko_TP_V.jpg', picture_comment: 'cat', picture_tag1_id: '1', picture_tag2_id: '2', picture_tag3_id: '3')
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
